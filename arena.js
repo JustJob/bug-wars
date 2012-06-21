99f53100760496252a9b72ce23c7e72950697a55
Justin O'Brien <justinobrien07@gmail.com>
2012-06-20 23:59:17 -0500

    completed move function... also added utility functions such as destory to objects

--- arena.js
+++ arena.js
@@ -1,4 +1,6 @@
 var DEFAULT_BUG_ENERGY = 40;
+var ENERGY_PER_SPACE = 5;
+var COMBINATION_COST = 10;
 
 //utility functions
 function forEachIn(object, action) {
@@ -10,7 +12,14 @@ function forEachIn(object, action) {
 
 //bugger arena
 function BugArena(p_initMap) {
+  if(p_initMap.length < 1)
+    throw "Invalid map size. Height < 1";
+  for(row p_initMap) {
+    if(row.length < 1) throw "Invalid map size. Width of a row < 1";
+  }
   this._map = this.createMap(p_initMap);
+  this.height = p_initMap.length;
+  this.width = p_initMap[0].length;
 }
 
 BugArena.prototype.createMap = function(p_2dArray) {
@@ -74,15 +83,61 @@ BugArena.prototype.getObjectFromLetter = function(x,y,letter) {
 };
 
 BugArena.prototype.move = function(x1,y1,x2,y2,playerId) {
-  if(this.mapHasBug(x1,y1,playerId)) {
+  var bug = this.mapHasBug(x1,y1,playerId)
+  if(bug && this.distance(x1,y1,x2,y2) < 2) {
+    var destinationObject = getObjectAt(x2,y2);
+    bug.energy -= ENERGY_PER_SPACE;
     
+    //move to empty space
+    if(!destinationObject) {
+      this._map[y1][x1] = undefined;
+      
+      if(bug.energy > 0) {
+        bug.x = x2;
+        bug.y = y2;
+        this._map[y2][x2] = bug;
+      }
+      else {
+        bug.destroy();
+      }
+    }
+    else if(destinationObject.prototype.constructor === Bug)
+    {
+      //move onto your own bug
+      if(destinationObject.playerId === playerId)
+      {
+        destinationObject += bug.energy - COMBINATION_COST;
+        bug.destroy();
+      }
+      //move onto enemy bug
+      else
+      {
+        if(bug.energy > destinationObject.energy) {
+          bug.energy -= destinationObject.energy;
+          destinationObject.destroy();
+        }
+        else if(destinationObject.energy) {
+          destinationObject.energy -= bug.energy;
+          bug.destroy();
+        }
+        else {
+          bug.destroy();
+          destinationObject.destroy();
+        }
+      }
+    }
   }
 };
 
+BugArena.prototype.getObjectAt = function(x,y) {
+  return this._map[y][x];
+};
+
 BugArena.prototype.mapHasBug = function(x,y,playerId) {
-  if(typeof this._map[y][x] === "object" && this._map[y][x].playerId)
+  var objectAt = this.getObjectAt(x,y);
+  if(typeof objectAt === "object" && objectAt.prototype.constructor === Bug && objectAt.playerId === playerId)
   {
-    return this._map[y][x].playerId;
+    return objectAt;
   }
   return undefined;
 };
@@ -94,12 +149,29 @@ var Bug = function(x,y,playerId) {
   this.energy = DEFAULT_BUG_ENERGY;
 };
 
+BugArena.prototype.destroy = function(object) {
+  if(object.x > 0 && object.x < this.width && object.y > 0 && object.y < this.height)
+  {
+    if(this.getObjectAt(object.x, object.y) == object) {
+      this._map[object.y][object.x] = undefined;
+      object.x = undefined;
+      object.y = undefined;
+    }
+    else throw "trying to destroy object but x and y coordinates did not align with the map";
+  }
+  else throw "trying to destory object in invalid position";
+};
+
 Bug.prototype.getHtml = function() {
   retval = "<td class=\"column" + this.y + " background-bug " + 'player' + this.playerId + "\"><span>";
   retval += this.energy;
   retval += "</span></td>";
   return retval;
 };
+
+Bug.prototype.destroy = function() {
+  BugArena.destroy(this);
+};  
 
 var Wall = function(x,y) {
   this.x = x;
