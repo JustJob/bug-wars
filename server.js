461617dcf0627ea862a32ecdd0b0f0463cfe7863
Justin O'Brien <justinobrien07@gmail.com>
2012-07-01 17:50:23 -0500

    removed session cookie stuff

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ var secret = connect.utils.uid(50);
 var connectApp = connect()
   .use(connect.logger('dev'))
   .use(connect.favicon())
-  .use(connect.cookieParser(secret))
-  .use(connect.session({ cookie: { maxAge: 60000 }}))
   .use(require("./RequestHandler").handleRequest);
 
 var app    = require("http").createServer(connectApp);
